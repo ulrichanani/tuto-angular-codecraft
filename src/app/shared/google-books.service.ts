658f2bb58cc2b525e717abf7db3d0f2b5b7827d6
Ulrich Anani <ulrichanan@gmail.com>
2018-09-06 14:11:09 +0100

    Integrate the source codes and librairies (jquery, bootstrap)

---
+++ src/app/shared/google-books.service.ts
@@ -0,0 +1,88 @@
+import {Injectable} from '@angular/core';
+import {Http} from '@angular/http';
+import {Book} from './book';
+import {map, tap} from 'rxjs/operators';
+
+@Injectable()
+export class GoogleBooksService {
+  private API_PATH: string = 'https://www.googleapis.com/books/v1/volumes';
+  public loading: boolean = false;
+  public initialised: boolean = false;
+  public totalItems: number = 0;
+  public _page: number = 1;
+  public pageSize: number = 10;
+  public query: string = '';
+  public books: Book[];
+
+
+  constructor(private http: Http) {
+  }
+
+  get startIndex() {
+    return this.page * this.pageSize;
+  }
+
+  get totalPages() {
+    try {
+      return Math.ceil(this.totalItems / this.pageSize);
+    } catch (e) {
+      console.error(e);
+      return 0;
+    }
+  }
+
+  get page(): number {
+    return this._page;
+  }
+
+  set page(val: number) {
+    if (val !== this.page) {
+      this._page = val;
+      this.searchBooks(this.query);
+    }
+  }
+
+
+  public searchBooks(queryTitle: string) {
+    this.query = queryTitle;
+    this.loading = true;
+    this.initialised = true;
+    this.books = [];
+    this.http.get(`${this.API_PATH}?q=${this.query}&maxResults=${this.pageSize}&startIndex=${this.startIndex}`)
+      .pipe(
+        map(res => res),
+        tap(res => {
+          let data: any = res;
+        this.totalItems = data.totadatalItems;
+      }),
+        map(res => {
+          let data: any = res;
+        return data.items ? data.items : [];
+      }),
+        map(items => {
+        return items.map(item => this.bookFactory(item));
+      }),
+        tap( () => this.loading = false),
+        tap(books => console.log(books))
+      ).subscribe((books) => this.books = books);
+  }
+
+  retrieveBook(bookId: string) {
+    return this.http.get(`${this.API_PATH}/${bookId}`);
+  }
+
+  private bookFactory(item: any): Book {
+    return new Book(
+      item.id,
+      item.volumeInfo.title,
+      item.volumeInfo.subtitle,
+      item.volumeInfo.authors,
+      item.volumeInfo.publisher,
+      item.volumeInfo.publishedDate,
+      item.volumeInfo.description,
+      item.volumeInfo.categories ? item.volumeInfo.categories.map((item) => item.split('/').pop().trim()) : ['N/A'],
+      item.volumeInfo.imageLinks.thumbnail,
+      item.volumeInfo.imageLinks.smallThumbnail
+    );
+  }
+}
